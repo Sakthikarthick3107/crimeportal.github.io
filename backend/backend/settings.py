56526f1a57d3e1f0bd173c44d9efb1c805c87ef2
Sakthikarthick3107 <sakthikarthick3107@gmail.com>
2023-03-28 00:06:56 +0530

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,7 +82,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
 EMAIL_HOST_USER = 'sakthikarthick3107@gmail.com'  
-EMAIL_HOST_PASSWORD = 'uznukaqwedaubjds'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
 
 
