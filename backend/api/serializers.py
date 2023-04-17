7d8c88bda6fc8792d15e5bf9c2d5865b47ba683c
Sakthikarthick <117448824+Sakthikarthick3107@users.noreply.github.com>
2023-04-18 00:22:28 +0530

    Delete backend directory

--- backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from . models import *
-
-class FileCaseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=FileCase
-        fields='__all__'
